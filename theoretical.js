55b97a4e0cb644a6411cc0fc98d88afc898f8b7a
Elton Hoffmann <elton.hoffmann@neoway.com.br>
2020-01-18 15:41:48 -0200

    ok thats it

---
+++ theoretical.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const scripts = document.getElementsByTagName("script");
+for (let i = 0; i < scripts.length; ++i) {
+  let script = scripts[i];
+  if (!script.src) {
+    const theoreticalGetter = new RegExp('JSON\.parse\(\((.*)\)\);');
+    const theoreticalGets = script.text.match(theoreticalGetter);
+    if (theoreticalGets && theoreticalGets.length) {
+      let data = theoreticalGets[1].slice(1,-1);
+      try {
+        data = JSON.parse(data);
+        data = JSON.parse(data);
+      } catch (e) {
+        console.error(e);
+        continue;
+      }
+
+      const lastTheoreticalPrice = data[data.length-1].TheoreticalPrice;
+      appendToBody(lastTheoreticalPrice);
+    }
+  }
+}
+
+function appendToBody(value) {
+  const h2 = document.createElement('h2');
+  h2.setAttribute("class", 'td-meu-investimento-titulo');
+  h2.appendChild(document.createTextNode("Valor total teórico bruto"));
+
+  const span1 = document.createElement('span');
+  span1.setAttribute("class", 'td-meu-investimento-titulo--valor');
+
+  const span2 = document.createElement('span');
+  span2.setAttribute("class", 'td-meu-investimento-titulo--cifrao');
+  span2.appendChild(document.createTextNode("R$"));
+
+  span1.appendChild(span2);
+  span1.appendChild(document.createTextNode(value));
+  h2.appendChild(span1);
+
+  document.querySelector('.td-meu-investimento').appendChild(h2);
+}
