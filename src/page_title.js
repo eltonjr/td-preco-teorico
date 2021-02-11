89949a1d31f893536ca4b56898145bd87d2c8382
Elton Hoffmann <elton.hoffmann@neoway.com.br>
2021-02-11 20:03:09 -0300

    Organizing src in functions

---
+++ src/page_title.js
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const rows = document.getElementsByClassName("td-posicao-detalhada__info");
+for (let i = 0; i < rows.length; ++i) {
+	let row = rows[i];
+	let detailsLink = row.lastElementChild;
+	
+	let href = detailsLink.getAttribute("href");
+	let res = content.fetch(href, {
+		credentials: 'same-origin'
+	});
+	res.then(r => r.text())
+		.then(t => {
+			const lastTheoreticalPrice = extractTheoreticalPrice(t);
+			if (lastTheoreticalPrice) {
+				console.log(lastTheoreticalPrice);
+				appendToTitleRow(row, lastTheoreticalPrice);
+			}
+		})
+		.catch(e => console.error(e));
+}
+
---
+++ src/theoretical.js
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const theoreticalGetter = new RegExp('JSON\.parse\(\((.*)\)\);');
+
+function extractTheoreticalPrice(script) {
+	const theoreticalGets = script.match(theoreticalGetter);
+	if (theoreticalGets && theoreticalGets.length) {
+		let data = theoreticalGets[1].slice(1,-1);
+		try {
+			data = JSON.parse(data);
+			data = JSON.parse(data);
+		} catch (e) {
+			console.error(e);
+			return "Invalid data";
+		}
+
+		return data[data.length-1].TheoreticalPrice;
+	}
+}
