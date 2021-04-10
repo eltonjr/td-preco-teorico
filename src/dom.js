d76a19bb23a44181b4572d9fe4bd12be7b4e98b9
Elton Hoffmann <elton.hoffmann@neoway.com.br>
2021-04-10 17:29:41 -0300

    Removing old details code

--- src/dom.js
+++ src/dom.js
@@ -2,25 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-function appendToDetailsBody(value) {
-	const h2 = document.createElement("h2");
-	h2.setAttribute("class", "td-meu-investimento-titulo");
-	h2.appendChild(document.createTextNode("Valor total teórico bruto"));
-
-	const span1 = document.createElement("span");
-	span1.setAttribute("class", "td-meu-investimento-titulo--valor");
-
-	const span2 = document.createElement("span");
-	span2.setAttribute("class", "td-meu-investimento-titulo--cifrao");
-	span2.appendChild(document.createTextNode("R$"));
-
-	span1.appendChild(span2);
-	span1.appendChild(document.createTextNode(value));
-	h2.appendChild(span1);
-
-	document.querySelector(".td-meu-investimento").appendChild(h2);
-}
-
 function appendToTitleRow(row, promise) {
 	const magicPosition = 5;
 
